9ccadd0a5644b27ef91e2117018bd5f35c91d1e1
ArinaKo <arinakovalenco7@gmail.com>
2020-07-27 15:56:10 +0700

    Создание сценария изменения персонажа

--- js/setup.js
+++ js/setup.js
@@ -4,14 +4,18 @@ var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'К
 var WIZARD_SECOND_NAMES = ['да Мария', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var WIZARD_COATS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var WIZARD_EYES = ['black', 'red', 'blue', 'yellow', 'green'];
+var WIZARD_FIREBALL = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
 
-var userSetup = document.querySelector('.setup');
-userSetup.classList.remove('hidden');
-
-var similarListElement = userSetup.querySelector('.setup-similar-list');
-
+var setup = document.querySelector('.setup');
+var setupOpen = document.querySelector('.setup-open');
+var setupClose = document.querySelector('.setup-close');
+var similarListElement = setup.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
-
+var userNameInput = document.querySelector('.setup-user-name');
+var userWizard = document.querySelector('.setup-player');
+var userWizardCoat = userWizard.querySelector('.wizard-coat');
+var userWizardEyes = userWizard.querySelector('.wizard-eyes');
+var userWizardFireball = userWizard.querySelector('.setup-fireball-wrap');
 
 var getRandomOption = function (options) {
   var i = Math.floor(Math.random() * options.length);
@@ -56,4 +60,69 @@ var renderWizardsList = function (place, list) {
 
 renderWizardsList(similarListElement, wizards);
 
-userSetup.querySelector('.setup-similar').classList.remove('hidden');
+setup.querySelector('.setup-similar').classList.remove('hidden');
+
+var onPopupEscPress = function (evt) {
+  if ((evt.key === 'Escape') && (document.activeElement !== setup.querySelector('.setup-user-name'))) {
+    evt.preventDefault();
+    setup.classList.add('hidden');
+  }
+};
+
+var openPopup = function () {
+  setup.classList.remove('hidden');
+
+  document.addEventListener('keydown', onPopupEscPress);
+};
+
+var closePopup = function () {
+  setup.classList.add('hidden');
+
+  document.removeEventListener('keydown', onPopupEscPress);
+};
+
+setupOpen.addEventListener('click', function () {
+  openPopup();
+});
+
+setupOpen.addEventListener('keydown', function (evt) {
+  if (evt.key === 'Enter') {
+    openPopup();
+  }
+});
+
+setupClose.addEventListener('click', function () {
+  closePopup();
+});
+
+setupClose.addEventListener('keydown', function (evt) {
+  if (evt.key === 'Enter') {
+    closePopup();
+  }
+});
+
+userNameInput.addEventListener('invalid', function () {
+  if (userNameInput.validity.valueMissing) {
+    userNameInput.setCustomValidity('You shall not pass. State your name.');
+  } else {
+    userNameInput.setCustomValidity('');
+  }
+});
+
+userWizardEyes.addEventListener('click', function () {
+  var color = getRandomOption(WIZARD_EYES);
+  userWizardEyes.style.fill = color;
+  setup.querySelector('[name=eyes-color]').value = color;
+});
+
+userWizardCoat.addEventListener('click', function () {
+  var color = getRandomOption(WIZARD_COATS);
+  userWizardCoat.style.fill = color;
+  setup.querySelector('[name=coat-color]').value = color;
+});
+
+userWizardFireball.addEventListener('click', function () {
+  var color = getRandomOption(WIZARD_FIREBALL);
+  userWizardFireball.style.background = color;
+  setup.querySelector('[name=fireball-color]').value = color;
+});
